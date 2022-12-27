f77a2922c96cc78c3efbceddae7592a3b3a56b2f
Javad Khajavi <javad_khajavi@yahoo.com>
2022-12-27 14:30:12 +0100

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -3,13 +3,14 @@ import './js/memory';
 import app from './js/app';
 import showTrips from './js/showTrips';
 
-// if there are trips in memory show trips if not add trip
+// Checks if there are any trips in the storage
 if (!localStorage.getObjectItem('trips') || localStorage.getObjectItem('trips').length < 1) {
+  // If no trips in storage, go to app page
   localStorage.setObjectItem('trips', []);
   app();
-  } else {
+} else {
   showTrips();
-  }
+}
 
 if (module.hot) {
   module.hot.accept();
