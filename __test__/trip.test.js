57b122f26ae7e3a14ca219c81f790f6c3b211441
Javad Khajavi <javad_khajavi@yahoo.com>
2022-12-28 11:50:42 +0100

    Update and rename fetch.spec.js to trip.test.js

---
+++ __test__/trip.test.js
@@ -0,0 +1,8 @@
+import Trip from '../src/client/js/input'
+
+describe('Trip model', () => {
+    it('A trip instance should be created', () => {
+        const newTrip = new Trip('city', 'country', 12, 13, {});
+        expect(newTrip.country).toEqual('country');
+    });
+})
